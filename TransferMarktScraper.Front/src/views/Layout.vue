6b4cab86e6a1dcfb94cfdd1ca5d487a0c5bbb6dd
Jesus <jesus.rodrigo@samelan.com>
2022-01-22 21:20:24 +0100

    layout clean

--- TransferMarktScraper.Front/src/views/Layout.vue
+++ TransferMarktScraper.Front/src/views/Layout.vue
@@ -3,19 +3,6 @@
         <Header></Header>
         <Footer></Footer>
         <router-view />
-        <!-- <Drawer></Drawer> -->
-
-        <!-- <q-page-container>
-            <q-page class="row no-wrap">
-                <div class="col">
-                    <div class="full-height">
-                        <q-scroll-area class="col q-pr-sm full-height" visible>
-                            <router-view />
-                        </q-scroll-area>
-                    </div>
-                </div>
-            </q-page>
-        </q-page-container> -->
     </q-layout>
 </template>
 
@@ -23,24 +10,17 @@
 
 import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
-import Drawer from '@/components/Drawer.vue'
 
 export default {
     name : 'Layout',
     components : {
         Footer,
-        Header,
-        Drawer
+        Header
     },
     data () {
-        return {
-            // drawer: false,
-            // menuItemSelected: null
-        }
     }
 }
 </script>
 
 <style scoped>
-
 </style>
