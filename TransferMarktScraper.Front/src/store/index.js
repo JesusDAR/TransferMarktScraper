c6272730a40f85b9251731eda7029215511bacb0
Jesus <jesus.rodrigo@samelan.com>
2022-02-20 17:36:03 +0100

    scrape players front request

--- TransferMarktScraper.Front/src/store/index.js
+++ TransferMarktScraper.Front/src/store/index.js
@@ -25,7 +25,18 @@ export default createStore({
   actions : {
     async scrapeTeams(context){
       return axios.get(process.env.VUE_APP_URL + "/teams/scrape").then((response) => {
-        let successes = response.data.results.filter( r => r.code === this.state.successCode ).map( r => r.message ).join('\r\n')      
+        let successes = response.data.results.filter( r => r.code === this.state.successCode ).map( r => r.message ).join('\r\n')    
+        let errors = response.data.results.filter( r => r.code === this.state.errorCode ).map( r => r.message ).join('\r\n')
+        context.commit('setSuccessOutput', this.state.successOutput.concat(successes))
+        context.commit('setErrorOutput', this.state.errorOutput.concat(errors))
+      }).catch( (error) => {
+        console.log(error)
+      })
+    },
+
+    async scrapePlayers(context, id){
+      return axios.get(process.env.VUE_APP_URL + "/players/scrape/teams/" + id ).then((response) => {
+        let successes = response.data.results.filter( r => r.code === this.state.successCode ).map( r => r.message ).join('\r\n')   
         let errors = response.data.results.filter( r => r.code === this.state.errorCode ).map( r => r.message ).join('\r\n')
         context.commit('setSuccessOutput', this.state.successOutput.concat(successes))
         context.commit('setErrorOutput', this.state.errorOutput.concat(errors))
